1a4d5fcaad4cd238046c3c629975f1b377c1a1d5
meszaros19 <meszaros19@lab103-10.iit.uni-miskolc.hu>
2021-10-06 08:48:34 +0200

    first test done

---
+++ boop/core/src/test/java/com/teszteles2021/core/AutoTest.java
@@ -0,0 +1,14 @@
+package com.teszteles2021.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AutoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
